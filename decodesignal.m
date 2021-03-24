92d6787375ed7273b0b7b745d739a49a26dc2032
ahmad <ahmad@mydealers.ca>
2021-03-23 23:18:20 -0400

    updated frame_search and power_calculation

--- decodesignal.m
+++ decodesignal.m
@@ -1,12 +1,18 @@
-function [result, cycles, output, gamma, RMS, SixHzSNNR]  = decodesignal(x, mode, max_cycles)
+function [result, cycles, output, gamma, RMS, SixHzSNNR, best_lag]  = decodesignal(x, mode, max_cycles)
 %n=45000; %number of samples
 n=length(x); %number of samples in x
 fs= 375; %sampling frequency
-spsymbol = [256, 512, 1024, 2048, 2560];
-interval = [2 4 8 16 20];
+spsymbol = [128, 256, 512, 1024, 2048, 2560];
+interval = [1 2 4 8 16 20];
 I=real(x');
 Q=imag(x');
 K=32;
-[result, cycles, output, gamma, RMS, SixHzSNNR]  = framesearch(...
-    I, Q, n,fs, 1, 0, 2, 16.25, spsymbol(mode), interval(mode), max_cycles);
+[result, cycles, output, gamma, RMS, SixHzSNNR, best_lag]  = framesearch(...
+    I, Q, n,fs, 1, 0, 16.25, spsymbol(mode), interval(mode), max_cycles);
 end
+
+
+
+% old signature
+% function [result, cycles, output, gamma, RMS, SixHzSNR] = framesearch(...
+%     I, Q, n, fs, tableindex, verbose, protocol_type, MINRMS, spsymbol, interval, max_cycles)
